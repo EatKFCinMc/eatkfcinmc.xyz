128b7c93a1a89ee7a0dc7531ddd1ddf660958435
EatKFCinMc <yuhaow29@uci.edu>
2026-07-14 15:50:23 -0700

    switching to amplify using astro-aws-amplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    adapter: awsAmplify({
+        customRules: [
+            { source: "/about/", target: "/about/index.html", status: "200" },
+            { source: "/blog/<slug>/", target: "/blog/<slug>/index.html", status: "200" },
+            { source: "/portfolio/", target: "/portfolio/index.html", status: "200" },
+        ]
+    }),
+    output: 'server',
+});
