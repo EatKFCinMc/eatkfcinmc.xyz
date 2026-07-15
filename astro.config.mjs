24a112b9773cd9fbed5e926919e238828bee7b66
EatKFCinMc <yuhaow29@uci.edu>
2026-07-14 22:54:10 -0700

    bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,11 @@ import awsAmplify from 'astro-aws-amplify';
 export default defineConfig({
     adapter: awsAmplify({
         customRules: [
+            { source: "/about", target: "/about/index.html", status: "200" },
             { source: "/about/", target: "/about/index.html", status: "200" },
+            { source: "/blog/<slug>", target: "/blog/<slug>/index.html", status: "200" },
             { source: "/blog/<slug>/", target: "/blog/<slug>/index.html", status: "200" },
+            { source: "/portfolio", target: "/portfolio/index.html", status: "200" },
             { source: "/portfolio/", target: "/portfolio/index.html", status: "200" },
         ]
     }),
